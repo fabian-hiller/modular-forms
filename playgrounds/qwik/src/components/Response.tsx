e91a07c56e9a46b4a045d29287eb8cc9803717cd
Fabian Hiller <hillerfabian11@gmail.com>
2023-03-15 13:39:17 +0100

    Refactor Response component of Qwik playground

--- playgrounds/qwik/src/components/Response.tsx
+++ playgrounds/qwik/src/components/Response.tsx
@@ -1,33 +1,45 @@
 import { component$ } from '@builder.io/qwik';
-import type { FormResponse } from '@modular-forms/qwik';
+import type { FormStore } from '@modular-forms/qwik';
 import clsx from 'clsx';
 import { useFrozenText } from '~/hooks';
 import { Expandable } from './Expandable';
 
-type ResponseProps = FormResponse;
+type ResponseProps = {
+  of: FormStore<any, any, any>;
+};
 
 /**
  * Response text usually used at the end of a form to provide feedback to the
  * user.
  */
-export const Response = component$(({ status, message }: ResponseProps) => {
-  // Freeze message while element collapses to prevent UI from jumping
+export const Response = component$(({ of: form }: ResponseProps) => {
+  // Freeze status and message while element collapses to prevent UI from
+  // jumping
+  const frozenStatus = useFrozenText(
+    {
+      get value() {
+        return form.response.status;
+      },
+    },
+    200
+  );
   const frozenMessage = useFrozenText(
     {
       get value() {
-        return message;
+        return form.response.message;
       },
     },
     200
   );
 
   return (
-    <Expandable expanded={!!message}>
+    <Expandable expanded={!!form.response.message}>
       <div
         class={clsx(
           'px-8 md:text-lg lg:px-10 lg:text-xl',
-          status === 'success' && 'text-emerald-500 dark:text-emerald-400',
-          status === 'error' && 'text-red-500 dark:text-red-400'
+          frozenStatus.value === 'success' &&
+            'text-emerald-500 dark:text-emerald-400',
+          frozenStatus.value === 'error' && 'text-red-500 dark:text-red-400'
         )}
       >
         {frozenMessage.value}
