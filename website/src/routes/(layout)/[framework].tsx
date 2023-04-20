f8a482bbb1782183e7c418bcf43bffaa5fa67ef0
Fabian Hiller <hillerfabian11@gmail.com>
2023-04-21 00:02:37 +0200

    Add class and meta tag for Algolia DocSearch

--- website/src/routes/(layout)/[framework].tsx
+++ website/src/routes/(layout)/[framework].tsx
@@ -1,14 +1,18 @@
 import { Show } from 'solid-js';
-import { Outlet, useParams } from 'solid-start';
+import { Meta, Outlet, useParams } from 'solid-start';
 import { Framework, frameworks } from '~/contexts';
 import NotFoundPage from './[...404]';
 
 export default function FrameworkLayout() {
+  // Use params
+  const params = useParams();
+
   return (
     <Show
-      when={frameworks.includes(useParams().framework as Framework)}
+      when={frameworks.includes(params.framework as Framework)}
       fallback={<NotFoundPage />}
     >
+      <Meta name="docsearch:framework" content={params.framework} />
       <Outlet />
     </Show>
   );
