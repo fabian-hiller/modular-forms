750b759604fd2ffd2e7f42af70f99280c4c8a6bf
Fabian Hiller <hillerfabian11@gmail.com>
2025-01-02 22:36:27 -0500

    Change dependency versions of website to fix bug

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,12 +1,14 @@
 import rehypePrism from '@mapbox/rehype-prism';
 import mdx from '@mdx-js/rollup';
 import rehypeSlug from 'rehype-slug';
-import solid from 'solid-start/vite'; // @ts-ignore
+import solid from 'solid-start/vite';
+// @ts-ignore
 import netlify from 'solid-start-netlify';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
+    // @ts-ignore
     {
       ...mdx({
         jsx: true,
